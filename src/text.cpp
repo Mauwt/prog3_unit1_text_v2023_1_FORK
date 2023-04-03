b948e63a14af89c3d9cf4fcc1bcadb0adbd7348e
maujacobo <codemauricio.jr@gmail.com>
2023-04-02 19:03:00 -0500

    Todo test pasados

--- src/text.cpp
+++ src/text.cpp
@@ -6,4 +6,99 @@
 #include "text.h"
 using namespace std;
 
+//Cosntructore
+Text::Text (const Text& other){
+    len = other.len;
+    _text = new char[len +1];
+    strcpy(_text, other._text);
+}
+
+Text::Text (const char* _str){ //Constructor por parametros
+    len = strlen(_str);
+    _text = new char[len + 1];
+    strcpy(_text, _str);
+}
+
+Text::Text (std::string& str){
+    len = str.length();
+    _text = new char[len +1];
+    std::strcpy(_text, str.c_str());
+}
+
+Text::Text(std::string&& _str){
+    len = _str.length();
+    _text = new char[len + 1];
+    std::strcpy(_text, _str.c_str());
+}
+
+Text::Text(Text&& other) noexcept{
+    len = other.len;
+    std::swap(_text, other._text);
+    std::swap(len, other.len);
+}
+
+//Asignacion por sobrecarga
+
+Text& Text::operator=(const Text& other) {
+    if (this != &other) {
+        delete[] _text;
+        len = other.len;
+        _text = new char[len + 1];
+        strcpy(_text, other._text);
+    }
+    return *this;
+}
+
+Text& Text::operator = (std::string& _str){
+    delete[] _text;
+    len = (int)_str.length();
+    _text = new char[len+1];
+    std::strcpy(_text, _str.c_str());
+    return *this;
+}
+Text::operator std::string() const {
+    return string(_text);
+}
+
+//Sobrecarga del operador ^ y ^=
+
+Text Text::operator^(const Text &other) {
+    int len2 = len + other.len;
+    char* result = new char[len2 + 1];
+    strcpy(result, _text);
+    strcat(result, other._text);
+    Text temp(result);
+    delete[] result;
+    return temp;
+}
+
+Text &Text::operator ^=(Text &other) {
+    int len2 = other.len;
+    len += len2;
+    char* result = new char[len + 1];
+    std::strcpy(result, other._text);
+    std::strcat(_text, result);
+    delete[] result;
+    return *this;
+}
+
+Text &Text::operator^=(const string &_str) {
+    len += _str.length();
+    char* temp = new char[len + 1] ;
+    std::strcpy(temp, _text);
+    std::strcat(temp, _str.c_str());
+
+    delete[] _text;
+    _text = new char[len +1];
+    std::strcpy(_text, temp);
+    delete[] temp;
+    return *this;
+}
+
+
+
+
+
+
+
 
--- src/text.h
+++ src/text.h
@@ -2,7 +2,73 @@
 // Created by rudri on 10/11/2020.
 //
 
+#include <cstring>
+#include <iostream>
+
 #ifndef PROG3_UNIT1_TEXT_V2023_1_TEXT_H
 #define PROG3_UNIT1_TEXT_V2023_1_TEXT_H
+
+class Text {
+private:
+    char* _text = nullptr;
+    int len = 0;
+public:
+
+    Text() = default;
+
+    Text(const Text& other);//Constructor copy
+
+    Text(const char* _str); // Constructor por parametros
+    Text(std::string& _str); // Constructor por parametros tipo string
+    Text(std::string&& _str); //Constructor por parametros tipo string rvalue
+    Text(Text&& other) noexcept; // Constructor move
+
+    //Asignación por sobrecarga
+
+    operator std::string() const;
+    Text& operator=(const Text& other);
+    Text& operator = (std::string& _str);
+    Text operator^ (const Text& other);
+    Text& operator ^= (Text& other);
+    Text& operator ^= (const std::string& _str);
+
+
+    //Getters
+    char* getText() { return _text; }
+    int getLen() { return len;}
+
+    //Setters
+    void setLen(int _len){
+        len = _len;
+    }
+
+    void display() {
+        for (int i = 0; i < len; i++) {
+            std::cout << _text[i];
+        }
+        std::cout << '\n';
+    }
+
+    //Funciones amigas
+    friend std::istream& getline(std::istream& in, Text& _t){
+        std::string temp;
+        std::getline(in, temp);
+        _t = temp;
+        return in;
+    }
+    friend std::ostream& operator<<(std::ostream& out, const Text& text){
+        out << std::string(text._text) <<" ";
+        return out;
+    }
+    friend std::istream& operator>>(std::istream& in, Text& text){
+        std::string _str;
+        std::getline(std::cin, _str);
+
+        Text temp(_str);
+        text = temp;
+        return in;
+    }
+};
+
 
 #endif //PROG3_UNIT1_TEXT_V2023_1_TEXT_H
